0be4bf1bb84e6cff5fab3ddf480dd9a6a2b2cc5f
Andrew Xiong <andrewbarf123@gmail.com>
2025-05-15 17:15:27 -0500

    Spring Boot 3.4.5 skeleton generated via Initializr

---
+++ backend/src/test/java/ai/andrew/medicare_backend/ai/andrew/medicare_backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.andrew.medicare_backend.ai.andrew.medicare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
